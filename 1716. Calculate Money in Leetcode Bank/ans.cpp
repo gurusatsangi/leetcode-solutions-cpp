ffefd94d50d4b2d5c65267c671e04a5859eb138b
gurusatsangi <guruprasadsatsangi15@gmail.com>
2025-11-27 21:33:36 +0530

    added 1716

---
+++ 1716. Calculate Money in Leetcode Bank/ans.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdcc++.h>
+using namespace std;
+class Solution {
+public:
+    int totalMoney(int n) {
+        int week=n/7;
+        int days=n%7;
+        int sum=0;
+        for(int i=0; i<week; i++){
+            sum+=28+7*i;
+        }
+        int start=week+1;
+        for(int i=0; i<days;i++){
+            sum+=start+i;
+        }
+        return sum;
+    }
+};
